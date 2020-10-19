163e736ba4d84cd953dac2ba7e8471efd4ebe566
jorgechauxjr <chauxjorge@hotmail.com>
2020-10-19 16:49:50 -0500

    Modify function type Tuple 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,10 +3,10 @@
 type-annotated function to_kv that takes a string k and an int OR float
 v as arguments and returns a tuple.
 """
-from typing import List, Union
+from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ returns a tuple"""
     my_tuple = (k, v * v)
     return my_tuple
