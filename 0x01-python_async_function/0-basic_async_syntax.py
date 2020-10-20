b41a1ccd9a1f8fe1846b8b46e48ba98f4a3fe10e
jorgechauxjr <chauxjorge@hotmail.com>
2020-10-20 16:48:31 -0500

    Add type of return 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """waits for a random delay between 0 and max_delay
     (included and float value) seconds and eventually returns it."""
     rand_number = random.uniform(0, max_delay)
