a57603ed75a8f2d4b942651dab8b975431477d1a
jorgechauxjr <chauxjorge@hotmail.com>
2020-10-20 17:32:53 -0500

    Add parameters type 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-
 """
 You will spawn wait_random n times
 with the specified max_delay.
 """
-
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay, n) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays (float values)"""
     delay_list: List[float] = []
     for i in range(n):
