a65e30380bd107ea09d82ce7a3e96fc6b5d470dc
jorgechauxjr <chauxjorge@hotmail.com>
2020-10-26 09:44:12 -0500

    Add 0-basic_cache.py

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Module for BasicCache class
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+     inherits from BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
