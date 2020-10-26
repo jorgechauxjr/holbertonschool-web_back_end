ba5d21582e5bd2d505ae89e4eca3e5093e5023fd
jorgechauxjr <chauxjorge@hotmail.com>
2020-10-26 11:04:52 -0500

    Add final 1-fifo_cache

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -33,6 +33,7 @@ class FIFOCache(BaseCaching):
             print("DISCARD: {}".format(to_discard))
             del self.cache_data[to_discard]
 
+        self.all_keys.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
