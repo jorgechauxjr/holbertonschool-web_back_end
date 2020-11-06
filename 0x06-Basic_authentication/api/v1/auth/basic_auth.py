48ea607c03db15977a6b0839fe1246a563a357ed
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-06 18:03:26 -0500

    Update basic_auth.py 8. Basic - Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic Authentication"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,23 @@ class BasicAuth(Auth):
         if authorization_header[0:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            str_bytes = base64.b64decode(base64_bytes)
+            the_str = str_bytes.decode('utf-8')
+            return the_str
+        except Exception:
+            return None
