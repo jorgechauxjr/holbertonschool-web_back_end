8c6717e7a578a1ce78ef21b822849ad824ca3420
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-11 19:28:48 -0500

    Task4 Update api/v1/auth/auth.py

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Class to manage the API authentication
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -36,3 +37,12 @@ class Auth():
         """Return None
         """
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        name_var = os.getenv('SESSION_NAME')
+        value = request.cookies.get(name_var) 
+        return value
