34811833b8cc3596ee7cc0a363af50e2fe8b8808
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-11 19:14:16 -0500

    Task2 Update session_auth.py

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 """first step for creating a new authentication mechanism
 """
-
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """that inherits from Auth
     """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
