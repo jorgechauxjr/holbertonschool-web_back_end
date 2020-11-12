733499babc552e6f1594763b85fee36df6509c93
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-12 16:03:18 -0500

    Mod t8 api/v1/auth/session_auth.py api/v1/views/session_auth.py

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -8,7 +8,7 @@ from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def sessionLogin() -> str:
+def login():
     """ POST /auth_session/login
     Return: Logged in user
     """
