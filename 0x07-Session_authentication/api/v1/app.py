c4128cbe191b0f90a338effda027af3a8ea6cc09
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-11 19:34:41 -0500

    Task5 Update api/v1/app.py

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -33,10 +33,11 @@ def beforeRequest():
     if auth is None:
         return
     paths_list = ['/api/v1/status/', '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/']
+                  '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, paths_list):
         return
-    if auth.authorization_header(request) is None:
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
         abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
