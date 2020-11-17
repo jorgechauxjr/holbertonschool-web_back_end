d4914f46f32877d82320776b99cf3bf3106fc323
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-17 10:03:13 -0500

    tk0 Add comments user.py

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-from sqlalchemy.ext.declarative import declarative_base
+"""
+SQLAlchemy model named User for a database table named users
+"""
 from sqlalchemy import Column, String, Integer
-
+from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    """SQLAlchemy model named User for a database table named users
+    """
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
