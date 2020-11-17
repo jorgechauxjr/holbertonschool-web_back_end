8780511fb6bf0ed113b6efb0bbc6e5ebb283236a
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-17 10:40:15 -0500

    tk2 Find user add 2-main.py Update db.py

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -45,3 +45,21 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """takes in arbitrary keyword arguments and
+        returns the first row found in the users table
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """takes in arbitrary keyword arguments and
+        returns the first row found in the users table
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
