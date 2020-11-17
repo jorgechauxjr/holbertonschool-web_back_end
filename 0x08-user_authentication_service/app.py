93c2fa0ee324e0131141a999a006b08c43f62457
jorgechauxjr <chauxjorge@hotmail.com>
2020-11-17 17:35:59 -0500

    Modify app.py 11login

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -40,13 +40,12 @@ def login():
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
-        response = jsonify({'email': email, 'message': 'logged in'})
-        response.set_cookie('session_id', session_id)
-        return response
-    else:
+    if not AUTH.valid_login(email, password):
         abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
