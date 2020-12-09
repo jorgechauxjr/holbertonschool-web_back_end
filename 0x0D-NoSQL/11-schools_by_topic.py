8b0d9de55951bd5451139182c0051484af5987d3
jorgechauxjr <chauxjorge@hotmail.com>
2020-12-08 16:25:50 -0800

    Add 11-schools_by_topic.py

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/USR/BIN/PYTHON3
+"""11. WHERE CAN I LEARN PYTHON?
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic:
+    -mongo_collection will be the pymongo collection object
+    -topic (string) will be topic searched
+    """
+
+    list_of_school = mongo_collection.find({"topics": topic})
+    return list_of_school
