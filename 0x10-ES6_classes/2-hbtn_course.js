1acf25b981f05c209edc301c4776f00e899a8ad2
jorgechauxjr <chauxjorge@hotmail.com>
2020-12-20 23:54:05 +0000

    Add 2-hbtn_course.js 2-main.js

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
+    if (Array.isArray(students) && students.every((val) => typeof (val) === 'string')) this._students = students;
+    else throw new Error(TypeError('Students must be an array of strings'));
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw TypeError('name must be a String');
+    this._length = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') throw TypeError('length must be a Number');
+    this._length = value;
+  }
+
+  set students(value) {
+    if (Array.isArray(value) && value.every((v) => typeof (v) === 'string')) this._students = value;
+    else throw new Error(TypeError('students must be an Array of Strings'));
+  }
+}
