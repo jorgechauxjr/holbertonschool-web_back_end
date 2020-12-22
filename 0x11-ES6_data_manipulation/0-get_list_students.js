e321793fb454e5328a38267e3e79fbee9bdfe88d
jorgechauxjr <chauxjorge@hotmail.com>
2020-12-22 10:47:54 -0500

    Mod return 0-get_list_students.js

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
 export default function getListStudents() {
-  return [
+  let obj_array = [
     { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
     { firstName: 'Serena', id: 5, location: 'San Francisco' },
   ];
+
+  return obj_array;
 }
