f68066734f1f152f6f5215ecf4a0288aa667752a
jorgechauxjr <chauxjorge@hotmail.com>
2020-12-22 15:51:32 +0000

    fix lint  0-get_list_students.js

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
-  let obj_array = [
+  const objArray = [
     { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
     { firstName: 'Serena', id: 5, location: 'San Francisco' },
   ];
 
-  return obj_array;
+  return objArray;
 }
