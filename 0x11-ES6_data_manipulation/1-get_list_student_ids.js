58d5036aac680c9ae307c2d5755eff4888c34442
jorgechauxjr <chauxjorge@hotmail.com>
2020-12-22 16:08:03 +0000

    Fix npm lint  1-get_list_student_ids.js

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
 export default function getListStudentIds(objArrayStudents) {
-  if (Array.isArray(objArrayStudents)){
+  if (Array.isArray(objArrayStudents)) {
     return objArrayStudents.map((student) => student.id);
   }
   return [];
-};
+}
