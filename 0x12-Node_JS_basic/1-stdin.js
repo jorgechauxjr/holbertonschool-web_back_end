a0e5d2397473cf5ec26af1f7e71ae4e241bf42f6
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-06 12:35:10 -0500

    Mod 1-stdin.js

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -4,8 +4,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+    if (name) process.stdout.write(`Your name is: ${name}\n`);
   }
 });
 // if stdout process in stream comes from the terminal, end process
-if (process.stdout.isTTY) process.stdin.on('end', () => process.stdout.write('This important software is now closing\n'));
+if (process.stdout.isTTY) {
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
