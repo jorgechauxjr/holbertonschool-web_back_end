91508319375f50e50958819145214375627397fe
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-06 17:06:24 +0000

    Del newLine 1-stdin.js

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
