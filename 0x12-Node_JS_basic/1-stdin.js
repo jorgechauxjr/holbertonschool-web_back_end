ac863aa37317c877d23150dc3d159dc777d103ab
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-06 19:25:42 +0000

    1-stdin.js Fix conditional

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -3,9 +3,7 @@ process.stdin.setEncoding('utf-8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name !== null) {
-    if (name) process.stdout.write(`Your name is: ${name}`);
-  }
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
