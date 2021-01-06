449fdc2ee21671385fc6dae93a7e6923b23713e5
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-06 12:37:06 -0500

    Rm new line 1-stdin.js

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-    if (name) process.stdout.write(`Your name is: ${name}\n`);
+    if (name) process.stdout.write(`Your name is: ${name}`);
   }
 });
 // if stdout process in stream comes from the terminal, end process
