67c235abfb1c4e6b02c49475d439bf938faa3106
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-06 17:28:07 +0000

    Change comments 1-stdin.js

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,3 +1,4 @@
+// Program that gets input from user and writes it to stdout
 process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
@@ -6,10 +7,5 @@ process.stdin.on('readable', () => {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
-
-/* process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name) process.stdout.write(`Your name is: ${name}\n`);
-    }); */
-
+// if stdout process in stream comes from the terminal, end process
 if (process.stdout.isTTY) process.stdin.on('end', () => process.stdout.write('This important software is now closing\n'));
