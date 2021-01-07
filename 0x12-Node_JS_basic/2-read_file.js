5b7ff070b745e4fe1bdac882670992c0d1f7418e
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-07 16:12:50 +0000

    Lint 2-readfile

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -10,28 +10,27 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
   try {
-      content = content.toString().split('\n');
+    content = content.toString().split('\n');
 
-      for (let i = 1; i < content.length; i += 1) {
-        const line = content[i].split(','); // get each word
-        if (inFields[line[3]]) { // line[3] is the field name
-          inFields[line[3]].counter += 1;
-          inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
-        } else {
-          inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
-        }
+    for (let i = 1; i < content.length; i += 1) {
+      const line = content[i].split(','); // get each word
+      if (inFields[line[3]]) { // line[3] is the field name
+        inFields[line[3]].counter += 1;
+        inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
+      } else {
+        inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
       }
-      console.log(`Number of students: ${content.length - 1}`);
-      for (const key in inFields) {
-        if (Object.prototype.hasOwnProperty.call(inFields, key)) {
-          console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
-        }
+    }
+    console.log(`Number of students: ${content.length - 1}`);
+    for (const key in inFields) {
+      if (Object.prototype.hasOwnProperty.call(inFields, key)) {
+        console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
       }
-      // console.log(content);
-      // console.log("Size: " + content.length);
-
-  } catch (e) { 
-    throw Error (e);
+    }
+    // console.log(content);
+    // console.log("Size: " + content.length);
+  } catch (e) {
+    throw Error(e);
   }
 }
 module.exports = countStudents;
