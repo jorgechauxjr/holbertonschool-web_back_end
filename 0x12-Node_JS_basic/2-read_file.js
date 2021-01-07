518608020d48e00d454abfa11bfaa8b3bf5c3770
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-07 15:59:10 +0000

    Fix lint 2-read_file.js

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  
   let content;
   const inFields = {}; // {field: {counter: # of students, students: [list of students in field]}}
-  
+
   try {
     content = fs.readFileSync(path);
   } catch (err) {
@@ -12,7 +11,7 @@ function countStudents(path) {
   }
   content = content.toString().split('\n');
 
-  for (let i = 1; i < content.length; i = i + 1) {
+  for (let i = 1; i < content.length; i += 1) {
     const line = content[i].split(','); // get each word
     if (inFields[line[3]]) { // line[3] is the field name
       inFields[line[3]].counter += 1;
@@ -22,11 +21,11 @@ function countStudents(path) {
     }
   }
   console.log(`Number of students: ${content.length - 1}`);
-    for (const key in inFields) {
-      if (Object.prototype.hasOwnProperty.call(inFields, key)) {
-        console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
-      }
+  for (const key in inFields) {
+    if (Object.prototype.hasOwnProperty.call(inFields, key)) {
+      console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
     }
+  }
   // console.log(content);
   // console.log("Size: " + content.length);
 }
