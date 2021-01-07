0800df93efeedec999c52c16df68a04532a7b48b
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-07 11:12:10 -0500

    Add try catch 2-read_file.js

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -9,24 +9,29 @@ function countStudents(path) {
   } catch (err) {
     throw new Error('Cannot load the database');
   }
-  content = content.toString().split('\n');
+  try {
+      content = content.toString().split('\n');
 
-  for (let i = 1; i < content.length; i += 1) {
-    const line = content[i].split(','); // get each word
-    if (inFields[line[3]]) { // line[3] is the field name
-      inFields[line[3]].counter += 1;
-      inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
-    } else {
-      inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
-    }
+      for (let i = 1; i < content.length; i += 1) {
+        const line = content[i].split(','); // get each word
+        if (inFields[line[3]]) { // line[3] is the field name
+          inFields[line[3]].counter += 1;
+          inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
+        } else {
+          inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
+        }
+      }
+      console.log(`Number of students: ${content.length - 1}`);
+      for (const key in inFields) {
+        if (Object.prototype.hasOwnProperty.call(inFields, key)) {
+          console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
+        }
+      }
+      // console.log(content);
+      // console.log("Size: " + content.length);
+
+  } catch (e) { 
+    throw Error (e);
   }
-  console.log(`Number of students: ${content.length - 1}`);
-  for (const key in inFields) {
-    if (Object.prototype.hasOwnProperty.call(inFields, key)) {
-      console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
-    }
-  }
-  // console.log(content);
-  // console.log("Size: " + content.length);
 }
 module.exports = countStudents;
