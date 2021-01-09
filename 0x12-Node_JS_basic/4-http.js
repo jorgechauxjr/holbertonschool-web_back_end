22ea9be36dd7a1adf2c639e06c7b06f6d14f8efb
jorgechauxjr <chauxjorge@hotmail.com>
2021-01-09 16:13:46 +0000

    lint 4-http.js

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,13 +1,14 @@
 // https://www.w3schools.com/nodejs/nodejs_http.asp
 
 const http = require('http');
+
 const port = 1245;
 
 // create a server object
-const app = http.createServer(function (req, res) {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello Holberton School!'); //write a response to the client
+  res.write('Hello Holberton School!'); // write a response to the client
   res.end(); // end the response
 });
 app.listen(port);
