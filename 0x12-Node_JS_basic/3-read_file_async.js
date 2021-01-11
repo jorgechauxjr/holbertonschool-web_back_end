dd2962ab9a91806799a2fdb42eb4ab6dbb9bcd46
Jorge <chauxjorge@hotmail.com>
2021-01-11 09:43:43 -0500

    Change t 3-read_file_async.js

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,35 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  const promise = (resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, dataDb) => {
-      if (err) reject(Error('Cannot load the database'));
-      if (dataDb) {
-        const inFields = {}; // {field: {counter: # of students, students: [list of students]}}
-        const data = dataDb.split('\n');
-        console.log(`Number of students: ${data.length - 1}`);
+module.exports = function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      try {
+        if (err) reject(Error('Cannot load the database'));
+        const totalData = data.split('\n').filter((line) => line).slice(1);
 
-        for (let i = 1; i < data.length; i += 1) {
-          const line = data[i].split(','); // get each word
-          if (inFields[line[3]]) { // line[3] is the field name
-            inFields[line[3]].counter += 1;
-            inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
-          } else {
-            inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
+        console.log(`Number of students: ${totalData.length}`);
+
+        let csCount = 0;
+        let csStudents = '';
+
+        let sweCount = 0;
+        let sweStudents = '';
+
+        totalData.forEach((student) => {
+          const info = student.split(',');
+          if (info[3] === 'CS') {
+            csCount += 1;
+            csStudents += csStudents ? `, ${info[0]}` : info[0];
+          } else if (info[3] === 'SWE') {
+            sweCount += 1;
+            sweStudents += sweStudents ? `, ${info[0]}` : info[0];
           }
-        }
-        for (const key in inFields) {
-          if (Object.prototype.hasOwnProperty.call(inFields, key)) {
-            console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
-          }
-        }
-        // ret obj of students in fields and number of students
-        resolve({ inFields, counter: data.length - 1 });
-      }
+        });
+
+        console.log(`Number of students in CS: ${csCount}. List: ${csStudents}`);
+        console.log(`Number of students in SWE: ${sweCount}. List: ${sweStudents}`);
+        return resolve();
+      } catch (err) { return reject(Error('Cannot load the database')); }
     });
-  };
-  return new Promise(promise);
-}
-module.exports = countStudents;
+  });
+};
